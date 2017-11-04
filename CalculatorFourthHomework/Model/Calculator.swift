dd11ede0d78dc982ca0a4c16cb06b7760da85bb9
frame <user@Users-MacBook-Pro.local>
2017-11-04 15:38:30 +0300

    Í hate xcode

---
+++ CalculatorFourthHomework/Model/Calculator.swift
@@ -0,0 +1,43 @@
+//
+//  Calculator.swift
+//  CalculatorFourthHomework
+//
+//  Created by User on 26/10/2017.
+//  Copyright © 2017 Studio Mobile. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    func subtract(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    func multiply(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    
+    func divide(a: Int, b: Int) throws -> Int {
+        guard b != 0 else {
+            throw NSError()
+        }
+        return a / b
+    }
+    
+    func operationStringRepresentation(a: Int, b: Int, operation: stringOperations) -> String{
+        return "\(a) " + operation.rawValue + "\(b)"
+    }
+    
+    enum stringOperations: String {
+        case add = " + "
+        case substract = " - "
+        case multiply = " * "
+        case divide = " / "
+    }
+
+}
+
